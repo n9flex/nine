2dbdf076bd06db99b56302726587707797e99208
nine <hashforce3@gmail.com>
2026-03-27 22:52:14 +0100

    ci: update version to v0.2.1

--- nine.ts
+++ nine.ts
@@ -14,7 +14,7 @@ import { listDir } from "./lib/storage";
 // SECTION: Constants
 // ============================================================================
 
-const VERSION = "0.2.0";
+const VERSION = "0.2.1";
 
 // ============================================================================
 // SECTION: Module Imports (Reconnaissance Modules - M2)
